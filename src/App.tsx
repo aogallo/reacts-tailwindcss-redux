3e495f30c2ae52e5ac64435a8c70b95da745a4f0
Allan Gallo <allan.gallo.guerra@gmail.com>
2023-10-18 10:31:46 -0600

    add props

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
           <TablePage />
         </Route>
         <Route path='/counter'>
-          <CounterPage />
+          <CounterPage initialCount={10} />
         </Route>
       </div>
     </div>
